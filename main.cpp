94830cf24325918bb320d19ef1c2368a25caec05
rockxxjin <rockxxjin@gmail.com>
2025-04-23 00:33:10 +0900

    refactor: Block 서브 클래스 제거하고 생성자 파라미터로 처리

--- main.cpp
+++ main.cpp
@@ -7,14 +7,6 @@
 #include <algorithm> // copy
 using namespace std;
 
-/*
-공백 = 0
-▦ = 1
-■ = 2
-이미 쌓인 블럭은 = 3
-블럭이 쌓이는 맨 밑바닥 = 4
-*/
-
 #define END_Y 6    // 게임 종료 선
 #define TABLE_X 15 //테트리스 판 x 축 길이
 #define TABLE_Y 38 //테트리스 판 y 축 길이
@@ -197,12 +189,27 @@ const int block5[4][4][4] = {
 
 /*블럭 부모 클래스*/
 class Block {
-protected:
+private:
     int shape[4][4][4]; // shape[rotate][y][x]
     int x; // x좌표
     int y; // y좌표
     int rotationCount; // shape[0][y][x], shape[1][y][x], shape[2][y][x], shaoe[3][y][x]로 4가지 상태 표현
 public:
+    Block() {
+       
+    }
+    Block(const int shape[4][4][4]) {
+        this->x= TABLE_X / 2 - 3;
+        this->y = 1;
+        this->rotationCount = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    this->shape[i][j][k] = shape[i][j][k]; // 블럭 객체 정보 저장
+                }
+            }
+        }
+    }
     int getShape(int rotationCount, int y, int x) {
         return shape[rotationCount][y][x];
     }
@@ -241,86 +248,6 @@ public:
     }
     void up() { //hard drop 처리용 블럭 한 칸 위로 이동
         y--;
-    }
-};
-/*1번 블럭 클래스*/
-class Block1 : public Block {
-public:
-    Block1() {
-        x = TABLE_X / 2 - 3; // 초기 생성 맨 위 중앙 쯤으로 맞춤
-        y = 1;
-        rotationCount = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    shape[i][j][k] = block1[i][j][k]; // 블럭 객체 정보 저장
-                }
-            }
-        }
-    }
-};
-/*2번 블럭 클래스*/
-class Block2 : public Block {
-public:
-    Block2() {
-        x = TABLE_X / 2 - 3; // 초기 생성 맨 위 중앙 쯤으로 맞춤
-        y = 1;
-        rotationCount = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    shape[i][j][k] = block2[i][j][k]; // 블럭 객체 정보 저장
-                }
-            }
-        }
-    }
-};
-/*3번 블럭 클래스*/
-class Block3 : public Block {
-public:
-    Block3() {
-        x = TABLE_X / 2 - 3; // 초기 생성 맨 위 중앙 쯤으로 맞춤
-        y = 1;
-        rotationCount = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    shape[i][j][k] = block3[i][j][k]; // 블럭 객체 정보 저장
-                }
-            }
-        }
-    }
-};
-/*4번 블럭 클래스*/
-class Block4 : public Block {
-public:
-    Block4() {
-        x = TABLE_X / 2 - 3; // 초기 생성 맨 위 중앙 쯤으로 맞춤
-        y = 1;
-        rotationCount = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    shape[i][j][k] = block4[i][j][k]; // 블럭 객체 정보 저장
-                }
-            }
-        }
-    }
-};
-/*5번 블럭 클래스*/
-class Block5 : public Block {
-public:
-    Block5() {
-        x = TABLE_X / 2 - 3; // 초기 생성 맨 위 중앙 쯤으로 맞춤
-        y = 1;
-        rotationCount = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    shape[i][j][k] = block5[i][j][k]; // 블럭 객체 정보 저장
-                }
-            }
-        }
     }
 };
 
@@ -420,11 +347,11 @@ public:
     void createBlock() {
         srand((unsigned int)time(NULL));
         int select = rand() % 5 + 1; // 1 ~ 5 블럭
-        if (select == 1) blockObject = new Block1(); // 1번 블럭 생성
-        else if (select == 2)blockObject = new Block2(); // 2번 블럭 생성
-        else if (select == 3)blockObject = new Block3(); // 3번 블럭 생성
-        else if (select == 4)blockObject = new Block4(); // 4번 블럭 생성
-        else if (select == 5)blockObject = new Block5(); // 5번 블럭 생성
+        if (select == 1) blockObject = new Block(block1); // 1번 블럭 생성
+        else if (select == 2)blockObject = new Block(block2); // 2번 블럭 생성
+        else if (select == 3)blockObject = new Block(block3); // 3번 블럭 생성
+        else if (select == 4)blockObject = new Block(block4); // 4번 블럭 생성
+        else if (select == 5)blockObject = new Block(block5); // 5번 블럭 생성
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
